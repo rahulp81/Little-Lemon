666aa505f8b6a9bc0b148e34c7005d8ea45be140
Rahul Pillai <rahulpillai81@gmail.com>
2023-06-02 20:18:29 +0530

    Components Refactoring and Editing

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Hero from "./Hero";
 import About from "./About";
 import Testimonials from "./Testimonials";
 import Footer from "./Footer";
-
+import Specials from "./Specials"
 
 export default function Home({bookingData}){
   return (
